8937923bc4554f7645698d14e75224346398b892
Eamon <eamonkummert@gmail.com>
2019-06-11 20:57:11 -0700

    Remove Useless and Commenting Unused Features

--- keyboards/crkbd/keymaps/bfgiant/keymap.c
+++ keyboards/crkbd/keymaps/bfgiant/keymap.c
@@ -31,15 +31,10 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
-  BACKLIT,
   RGBRST
 };
 
-enum macro_keycodes {
-  KC_SAMPLEMACRO,
-};
-
-#define KC______ KC_TRNS
+#define KC______ KC_TRANSPARENT
 #define KC_XXXXX KC_NO
 #define KC_LOWER LOWER
 #define KC_RAISE RAISE
@@ -138,31 +133,27 @@ void matrix_init_user(void) {
 
 // When add source files to SRC in rules.mk, you can use functions.
 const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
+// const char *read_logo(void);
+// void set_keylog(uint16_t keycode, keyrecord_t *record);
+// const char *read_keylog(void);
+// const char *read_keylogs(void);
 
 void matrix_scan_user(void) {
    iota_gfx_task();
 }
 
 void matrix_render_user(struct CharacterMatrix *matrix) {
+  // display different things on each side
   if (is_master) {
     // If you want to change the display of OLED, you need to change here
     matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
-    matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
+    // matrix_write_ln(matrix, read_keylog()); // shows last letter that was pressed
+    // matrix_write_ln(matrix, read_keylogs()); // shows last series of letters pressed
+    // matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+    // matrix_write_ln(matrix, read_host_led_state());
+    // matrix_write_ln(matrix, read_timelog());
   } else {
-    matrix_write(matrix, read_logo());
+    // matrix_write(matrix, read_logo());
   }
 }
 
@@ -182,13 +173,6 @@ void iota_gfx_task_user(void) {
 #endif//SSD1306OLED
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-#ifdef SSD1306OLED
-    set_keylog(keycode, record);
-#endif
-    // set_timelog();
-  }
-
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
